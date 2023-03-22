5f73adeecef7d7b2622c71601d29347a0ee11633
M. Wahid Maulana <wahidmaulana236@gmail.com>
2023-03-22 08:03:27 +0700

    Make Card with html,CSS, and Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnTwitter = document.querySelector(".btn-twitter");
+const btnGithub = document.querySelector(".btn-github");
+
+btnTwitter.addEventListener("click", () => {
+  window.open("https://twitter.com/wahiidMaulana");
+});
+
+btnGithub.addEventListener("click", () => {
+  window.open("https://github.com/wahidmaulana");
+});
